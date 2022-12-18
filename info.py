0dff17675feb53cb586f6a76a1eaaa3403807973
Cynite <105969534+CyniteOfficial@users.noreply.github.com>
2022-12-18 11:54:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,8 +49,7 @@ MSG_ALRT = environ.get('MSG_ALRT', 'Share and Support Us')
 
 # Custom Chats
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', -1001797596826))
-FILE_CHANNEL = int(environ.get('FILE_CHANNEL', -1001810806290)) 
-)
+FILE_CHANNEL = int(environ.get('FILE_CHANNEL', -1001810806290))
 FILE_CHANNEL_LINK = environ.get('FILE_CHANNEL_LINK', 't.me/cynitebackup')
 HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://t.me/cynitemovies/3')
 
