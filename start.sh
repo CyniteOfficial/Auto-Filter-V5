f4a37dab6fdd088651463d99e6753b28f131bf26
AS-WORLD-OFFICIAL <118078136+AS-WORLD-OFFICIAL@users.noreply.github.com>
2022-12-25 00:21:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Cyniteofficial/Auto-Filter-V5.git /Auto-Filter-V5
+  git clone https://github.com/Asworldofficial/As-movie-provider.git /As-movies-provider
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Auto-Filter-V5
 fi
-cd /Auto-Filter-V5
+cd /As-movies-provider
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
