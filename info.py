8effb4c6d40e45b189c36a485b8e0693f2d04be2
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-06-23 20:12:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,8 +37,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Singhsawan:singh@cluster0.g1lpyz0.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Trsge")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://nikhilmaurya3214:sawan09@cluster0.4prggx4.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Channel Button Links
